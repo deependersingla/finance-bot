5b7a08c9d938d7c1dcf3da46bd24003c0e3f278d
deepender <deepender281190@gmail.com>
2016-05-08 13:57:29 +0530

    working query

--- request.py
+++ request.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from string_search import *
+from flask import request
+import pdb
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/search')
+def search():
+	return find_string(request.args.get("query"))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
