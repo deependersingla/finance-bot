1ec2667a8453d539d93eaaba856ed88e855a3e34
Deepender <deepender281190@gmail.com>
2016-05-08 19:51:04 +0530

    data from scrrener

--- string_search.py
+++ string_search.py
@@ -1,8 +1,30 @@
 from fuzzywuzzy import process
 from fuzzywuzzy import fuzz
+import csv
+import json
+import requests
+import urllib
 
 #todo change lines to be a global constant and one time load
 def find_string(string):
 	with open('whitelabelled_words.txt') as f:
 		lines = f.read().splitlines()
 	return process.extractOne(string, lines)[0]
+
+
+def get_screener_data(company,ratio):
+    company_dictionary = {}
+    with open('company_data.csv', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            company_dictionary[row[0]] = row[1]
+    company_bse_name = company_dictionary[company]
+    url = "https://www.screener.in/api/company/" + company_bse_name
+    r = requests.get(url)
+    company_id = r.json()["warehouse_set"]["id"]
+    query = {"q": ratio}
+    #todo write method to update this from cache or Ram
+    cookie = {'sessionid': 'gqnbs0gobw18pzykfyawuhned3pwqy5r'}
+    url  =  "https://www.screener.in/api/company/" + str(company_id) + "/ratio/?" + urllib.urlencode(query)
+    r = requests.get(url, cookies=cookie)
+    return r.json()["ratio"][1]
